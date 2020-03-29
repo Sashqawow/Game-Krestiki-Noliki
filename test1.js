920a81797bb2bf10075f7c5bcee0cdbd17114d1c
Alexander <62546590+Sashqawow@users.noreply.github.com>
2020-03-29 20:08:07 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,36 +1,30 @@
 
 alert('Добро пожаловать в игру "Крестики-нолики"');
 function validateForm() {
-        var x = document.forms["myForm"]["1name"].value;
-        var y = document.forms["myForm"]["2name"].value;
-        if (x == ""|| x==" ") {
-            alert("Необходимо ввести имя первого игрока");
-            return false;
-        }
-        
-        if (y == ""|| y==" ") {
-            alert("Необходимо ввести имя второго игрока");
-            return false;
-        }else{
-        
-                        for (var i=0; i<9; i++){
-                        document.getElementById('krestik').innerHTML+='<div class="block"></div>'; 
-                           
-    
-
+var firstPlayerName = document.forms["myForm"]["1name"].value;
+var secondPlayerName = document.forms["myForm"]["2name"].value;
+if (firstPlayerName == false) {
+  alert("Необходимо ввести имя первого игрока");
+  return false;
+}
+if (secondPlayerName==false) {
+  alert('Необходимо ввести имя второго игрока');
+}else{
+ for (var i=0; i<9; i++){
+ document.getElementById('krestik').innerHTML+='<div class="block"></div>'; 
+                             
+         
 var element=0;
 document.getElementById('krestik').onclick=function(click){
-        if (click.target.className=='block'){
-                if (element%2==0){
-                click.target.innerHTML='X';
-                }else{
-                click.target.innerHTML='O';
-                
-                }
-                element++;
-                win();
-                
-        }
+if (click.target.className=='block'){
+  if (element%2==0){
+  click.target.innerHTML='X';
+  }else{
+  click.target.innerHTML='O';
+  }
+  element++;
+  win();       
+}
 }
 
 
@@ -90,6 +84,6 @@ function win (){
 }
 }        
 }
-}    
+}   
      
 
