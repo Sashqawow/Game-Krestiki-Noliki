adb0177be2d7175dbc2a9e27320c2b8f16b20500
Alexander <62546590+Sashqawow@users.noreply.github.com>
2020-04-06 16:47:56 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,6 +1,6 @@
-
 alert('Добро пожаловать в игру "Крестики-нолики"');
 function validateForm() {
+
 var firstPlayerName = document.forms["myForm"]["1name"].value;
 var secondPlayerName = document.forms["myForm"]["2name"].value;
 if (firstPlayerName == false) {
@@ -12,18 +12,17 @@ if (secondPlayerName==false) {
 }else{
  for (var i=0; i<9; i++){
  document.getElementById('krestik').innerHTML+='<div class="block"></div>'; 
-                             
-         
+
 var element=0;
 document.getElementById('krestik').onclick=function(click){
 if (click.target.className=='block'){
-  if (element%2==0){
-  click.target.innerHTML='X';
+  if (element==0){
+  click.target.innerHTML="X";
   }else{
-  click.target.innerHTML='O';
+  click.target.innerHTML="O";
   }
   element++;
-  win();       
+  win();  
 }
 }
 
@@ -31,7 +30,7 @@ if (click.target.className=='block'){
 function win (){
         var allelement=document.getElementsByClassName('block');
         if (allelement[0].innerHTML=='X'&&allelement[1].innerHTML=='X'&& allelement[2].innerHTML=='X'){
-                if (alert('Победил '+document.forms["myForm"]["1name"].value));     
+                (alert('Победил '+document.forms["myForm"]["1name"].value));     
         location.reload();}
         if (allelement[3].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[5].innerHTML=='X'){
                 alert('Победил '+document.forms["myForm"]["1name"].value);     
@@ -85,5 +84,4 @@ function win (){
 }        
 }
 }   
-     
-
+  
