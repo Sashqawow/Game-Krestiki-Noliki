158b8b3ec62f7e4648798751308813f25e20f81f
Sergey Guryev <Serg.gurjev@gmail.com>
2020-04-06 17:39:12 +0800

    firest step to do it right

--- test1.js
+++ test1.js
@@ -1,87 +1,76 @@
 alert('Добро пожаловать в игру "Крестики-нолики"');
-function validateForm() {
 
-var firstPlayerName = document.forms["myForm"]["1name"].value;
-var secondPlayerName = document.forms["myForm"]["2name"].value;
-if (firstPlayerName == false) {
-  alert("Необходимо ввести имя первого игрока");
-  return false;
-}
-if (secondPlayerName==false) {
-  alert('Необходимо ввести имя второго игрока');
-}else{
- for (var i=0; i<9; i++){
- document.getElementById('krestik').innerHTML+='<div class="block"></div>'; 
+var deck = [null, null, null, null, null, null, null, null, null]; // пустая доска, без X или O, начало игры
+// Players
+var firstPlayerName = null;
+var secondPlayerName = null;
 
-var element=0;
-document.getElementById('krestik').onclick=function(click){
-if (click.target.className=='block'){
-  if (element==0){
-  click.target.innerHTML="X";
-  }else{
-  click.target.innerHTML="O";
-  }
-  element++;
-  win();  
-}
-}
-
-
-function win (){
-        var allelement=document.getElementsByClassName('block');
-        if (allelement[0].innerHTML=='X'&&allelement[1].innerHTML=='X'&& allelement[2].innerHTML=='X'){
-                (alert('Победил '+document.forms["myForm"]["1name"].value));     
-        location.reload();}
-        if (allelement[3].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[5].innerHTML=='X'){
-                alert('Победил '+document.forms["myForm"]["1name"].value);     
-        location.reload();}
-        if (allelement[6].innerHTML=='X'&&allelement[7].innerHTML=='X'&& allelement[8].innerHTML=='X'){
-                alert('Победил '+document.forms["myForm"]["1name"].value);     
-        location.reload();}
-        if (allelement[0].innerHTML=='X'&&allelement[3].innerHTML=='X'&& allelement[6].innerHTML=='X'){
-                alert('Победил '+document.forms["myForm"]["1name"].value);     
-        location.reload();}
-        if (allelement[1].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[7].innerHTML=='X'){
-                alert('Победил '+document.forms["myForm"]["1name"].value);     
-        location.reload();}
-        if (allelement[2].innerHTML=='X'&&allelement[5].innerHTML=='X'&& allelement[8].innerHTML=='X'){
-                alert('Победил '+document.forms["myForm"]["1name"].value);     
-        location.reload();}
-        if (allelement[0].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[8].innerHTML=='X'){
-                alert('Победил '+document.forms["myForm"]["1name"].value);     
-        location.reload();}
-        if (allelement[2].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[6].innerHTML=='X'){
-                alert('Победил '+document.forms["myForm"]["1name"].value);     
-        location.reload();}
-        
-     
-        if (allelement[0].innerHTML=='O'&&allelement[1].innerHTML=='O'&& allelement[2].innerHTML=='O') {
-                alert('Победил '+document.forms["myForm"]["2name"].value);     
-        location.reload();}
-        if (allelement[3].innerHTML=='O'&&allelement[4].innerHTML=='O'&& allelement[5].innerHTML=='O') {
-                alert('Победил '+document.forms["myForm"]["2name"].value);     
-        location.reload();}
-        if (allelement[6].innerHTML=='O'&&allelement[7].innerHTML=='O'&& allelement[8].innerHTML=='O') {
-                alert('Победил '+document.forms["myForm"]["2name"].value);     
-        location.reload();}
-        if (allelement[0].innerHTML=='O'&&allelement[3].innerHTML=='O'&& allelement[6].innerHTML=='O') {
-                alert('Победил '+document.forms["myForm"]["2name"].value);     
-        location.reload();}
-        if (allelement[1].innerHTML=='O'&&allelement[4].innerHTML=='O'&& allelement[7].innerHTML=='O') {
-                alert('Победил '+document.forms["myForm"]["2name"].value);     
-        location.reload();}
-        if (allelement[2].innerHTML=='O'&&allelement[5].innerHTML=='O'&& allelement[8].innerHTML=='O') {
-                alert('Победил '+document.forms["myForm"]["2name"].value);     
-        location.reload();}
-        if (allelement[0].innerHTML=='O'&&allelement[4].innerHTML=='O'&& allelement[8].innerHTML=='O') {
-                alert('Победил '+document.forms["myForm"]["2name"].value);     
-        location.reload();}
-        if (allelement[2].innerHTML=='O'&&allelement[4].innerHTML=='O'&& allelement[6].innerHTML=='O') {
-                alert('Победил '+document.forms["myForm"]["2name"].value);     
-        location.reload();}
-        
-}
-}        
-}
-}   
+setPlayersNames() {
+  var name1 = document.forms["myForm"]["1name"].value.trim();
+  var name2 = document.forms["myForm"]["2name"].value.trim();
   
+  if (name1.length < 2 && name2.length < 2) {
+    var notification = docuemnt.createElement('p')
+    notification.classList.add("notification-warning");
+    notification.textContent = 'Введите корректные имена';
+    docuemnt.getElementByTag('h1').after(notification);
+  } else {
+    // TODO: start the game
+  }
+}
+
+ 
+
+  function win (){
+          var allelement=document.getElementsByClassName('block');
+          if (allelement[0].innerHTML=='X'&&allelement[1].innerHTML=='X'&& allelement[2].innerHTML=='X'){
+                  (alert('Победил '+document.forms["myForm"]["1name"].value));     
+          location.reload();}
+          if (allelement[3].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[5].innerHTML=='X'){
+                  alert('Победил '+document.forms["myForm"]["1name"].value);     
+          location.reload();}
+          if (allelement[6].innerHTML=='X'&&allelement[7].innerHTML=='X'&& allelement[8].innerHTML=='X'){
+                  alert('Победил '+document.forms["myForm"]["1name"].value);     
+          location.reload();}
+          if (allelement[0].innerHTML=='X'&&allelement[3].innerHTML=='X'&& allelement[6].innerHTML=='X'){
+                  alert('Победил '+document.forms["myForm"]["1name"].value);     
+          location.reload();}
+          if (allelement[1].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[7].innerHTML=='X'){
+                  alert('Победил '+document.forms["myForm"]["1name"].value);     
+          location.reload();}
+          if (allelement[2].innerHTML=='X'&&allelement[5].innerHTML=='X'&& allelement[8].innerHTML=='X'){
+                  alert('Победил '+document.forms["myForm"]["1name"].value);     
+          location.reload();}
+          if (allelement[0].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[8].innerHTML=='X'){
+                  alert('Победил '+document.forms["myForm"]["1name"].value);     
+          location.reload();}
+          if (allelement[2].innerHTML=='X'&&allelement[4].innerHTML=='X'&& allelement[6].innerHTML=='X'){
+                  alert('Победил '+document.forms["myForm"]["1name"].value);     
+          location.reload();}
+
+
+          if (allelement[0].innerHTML=='O'&&allelement[1].innerHTML=='O'&& allelement[2].innerHTML=='O') {
+                  alert('Победил '+document.forms["myForm"]["2name"].value);     
+          location.reload();}
+          if (allelement[3].innerHTML=='O'&&allelement[4].innerHTML=='O'&& allelement[5].innerHTML=='O') {
+                  alert('Победил '+document.forms["myForm"]["2name"].value);     
+          location.reload();}
+          if (allelement[6].innerHTML=='O'&&allelement[7].innerHTML=='O'&& allelement[8].innerHTML=='O') {
+                  alert('Победил '+document.forms["myForm"]["2name"].value);     
+          location.reload();}
+          if (allelement[0].innerHTML=='O'&&allelement[3].innerHTML=='O'&& allelement[6].innerHTML=='O') {
+                  alert('Победил '+document.forms["myForm"]["2name"].value);     
+          location.reload();}
+          if (allelement[1].innerHTML=='O'&&allelement[4].innerHTML=='O'&& allelement[7].innerHTML=='O') {
+                  alert('Победил '+document.forms["myForm"]["2name"].value);     
+          location.reload();}
+          if (allelement[2].innerHTML=='O'&&allelement[5].innerHTML=='O'&& allelement[8].innerHTML=='O') {
+                  alert('Победил '+document.forms["myForm"]["2name"].value);     
+          location.reload();}
+          if (allelement[0].innerHTML=='O'&&allelement[4].innerHTML=='O'&& allelement[8].innerHTML=='O') {
+                  alert('Победил '+document.forms["myForm"]["2name"].value);     
+          location.reload();}
+          if (allelement[2].innerHTML=='O'&&allelement[4].innerHTML=='O'&& allelement[6].innerHTML=='O') {
+                  alert('Победил '+document.forms["myForm"]["2name"].value);     
+          location.reload();}
+  }
